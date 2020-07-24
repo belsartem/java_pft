71d79e57bc5c2db23f8d83c2609737f565913f8e
Artem Bels <belsartem@gmail.com>
2020-07-24 14:46:50 +0300

    Добавлены несколько методов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,29 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args){
-		System.out.println("Hello world!");
+		hello("world");
+		hello("user");
+		hello("Artem");
+
+		double l = 5;
+		System.out.println("Square area with a side " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Rectangle area with sides " + a + " and " + " = " + area(a, b));
+
+	}
+
+	public static void hello(String somebody){
+		System.out.println("Hello " + somebody + "!");
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b){
+		return a * b;
 	}
 
 }
