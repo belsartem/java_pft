58bcd6f5d22b3e70ab6f8ab7e7022914f7514377
Artem Bels <belsartem@gmail.com>
2020-07-25 19:02:39 +0300

    Созданы методы Rectangle и Square

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,30 +2,20 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		hello("world");
 		hello("user");
 		hello("Artem");
 
-		double l = 5;
-		System.out.println("Square area with a side " + l + " = " + area(l));
+		Square s = new Square(5);
+		System.out.println("Square area with a side " + s.l + " = " + s.area());
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Rectangle area with sides " + a + " and " + " = " + area(a, b));
-
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Rectangle area with sides " + r.a + " and " + r.b + " = " + r.area());
 	}
 
 	public static void hello(String somebody){
 		System.out.println("Hello " + somebody + "!");
-	}
-
-	public static double area(double len) {
-		return len * len;
-	}
-
-	public static double area(double a, double b){
-		return a * b;
 	}
 
 }
