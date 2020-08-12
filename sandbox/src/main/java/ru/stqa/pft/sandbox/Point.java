1961e62d7afde282674ce5bdc0d64f2cfea4c8ed
Artem Bels <belsartem@gmail.com>
2020-08-12 15:37:25 +0300

    Исправление замечаний к Заданию №2
    Замечание было в том, что нужно удалить два допкласса PointOne и PointTwo и реализовать всё в одной классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,16 +2,29 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
+    double x1 = 5;
+    double y1;
+
+    double x2;
+    double y2;
+
+
     public static void main(String[] args) {
 
-        PointOne p1 = new PointOne(5, 3.12);
-        PointTwo p2 = new PointTwo(32.11, 15.5);
+        Point p1 = new Point();
+        p1.x1 = 5;
+        p1.y1 = 3.12;
 
-        System.out.println("The square root multiplication of two dots with coordinates: " + "A(" + (p1.x1) + ";" + (p1.y1) + ")" + " & " + "B(" + (p2.x2) + ";" + (p2.y2) + ")" + " equals to: " + distance(p1, p2));
+        Point p2 = new Point();
+        p2.x2 = 32.11;
+        p2.y2 = 15.5;
+
+    System.out.println("The square root multiplication of two dots with coordinates: " + "A(" + (p1.x1) + ";" + (p1.y1) + ")" + " & " + "B(" + (p2.x2) + ";" + (p2.y2) + ")" + " equals to: " + distance(p1, p2));
 
     }
 
-    public static double distance(PointOne p1, PointTwo p2) {
+    public static double distance(Point p1, Point p2) {
         return Math.sqrt(((p2.x2 - p1.x1) * (p2.x2 - p1.x1)) + ((p2.y2 - p1.y1) * (p2.y2 - p1.y1)));
+
     }
 }
