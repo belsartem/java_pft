e658346c82a50a6b89bfacce785d2e37edb7358f
Artem Bels <belsartem@gmail.com>
2020-08-23 14:27:41 +0300

    Задание 2. Исправление замечаний

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,20 +2,19 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
-		hello("world");
-		hello("user");
-		hello("Artem");
+    public static void main(String[] args) {
+        double x1 = 5;
+        double x2 = 3.12;
+        double y1 = 32.11;
+        double y2 = 15.5;
+        Point p1 = new Point(x1, y1);
+        Point p2 = new Point(x2, y2);
+        
+        System.out.println("Distance between two dots with coordinates: " + "A(" + x1 + ";" + y1 + ")" + " & " + "B(" + x2 + ";" + y2 + ") " + "equals to: " + distance(p1, p2));
+        System.out.println("Distance between two dots with coordinates: " + "A(" + p1.x + ";" + p1.y + ")" + " & " + "B(" + p2.x + ";" + p2.y + ") " + "equals to: " + p1.distance(p2));
+    }
 
-		Square s = new Square(5);
-		System.out.println("Square area with a side " + s.l + " = " + s.area());
-
-		Rectangle r = new Rectangle(4, 6);
-		System.out.println("Rectangle area with sides " + r.a + " and " + r.b + " = " + r.area());
-	}
-
-	public static void hello(String somebody){
-		System.out.println("Hello " + somebody + "!");
-	}
-
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
+    }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,29 +2,15 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-    double x1 = 5;
-    double y1;
+    public double x;
+    public double y;
 
-    double x2;
-    double y2;
-
-
-    public static void main(String[] args) {
-
-        Point p1 = new Point();
-        p1.x1 = 5;
-        p1.y1 = 3.12;
-
-        Point p2 = new Point();
-        p2.x2 = 32.11;
-        p2.y2 = 15.5;
-
-    System.out.println("The square root multiplication of two dots with coordinates: " + "A(" + (p1.x1) + ";" + (p1.y1) + ")" + " & " + "B(" + (p2.x2) + ";" + (p2.y2) + ")" + " equals to: " + distance(p1, p2));
-
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt(((p2.x2 - p1.x1) * (p2.x2 - p1.x1)) + ((p2.y2 - p1.y1) * (p2.y2 - p1.y1)));
-
+    public double distance(Point p2) {
+        return Math.sqrt((this.x - p2.x)*(this.x - p2.x) + (this.y - p2.y)*(this.y - p2.y));
     }
 }
